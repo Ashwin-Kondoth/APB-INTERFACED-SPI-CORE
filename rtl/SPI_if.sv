19c61df19f8bc1bfc71242594737c3c552e4608c
Ashwin-Kondoth <ashwinkondoth@gmail.com>
2026-05-08 21:18:41 +0530

    added and updated SPI interface file

---
+++ rtl/SPI_if.sv
@@ -0,0 +1,26 @@
+interface SPI_if (input bit clock);
+	logic ss;
+	logic sclk;
+	logic mosi;
+	logic miso;
+
+	clocking spi_drv_cb @(posedge clock);
+		default input #1 output #1;
+		output miso;
+		input ss;
+		input sclk;
+		input mosi;
+	endclocking : spi_drv_cb
+
+	clocking spi_mon_cb @(posedge clock);
+		default input #1 output #1;
+		input miso;
+		input ss;
+		input sclk;
+		input mosi;
+	endclocking : spi_mon_cb
+
+	modport SPI_DRV_MP (clocking spi_drv_cb);
+	modport SPI_MON_MP (clocking spi_mon_cb);
+
+endinterface : SPI_if

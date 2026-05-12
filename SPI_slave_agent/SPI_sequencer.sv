a3847add1977569392bcc0a1ace9f7d0ee95a83c
Ashwin-Kondoth <ashwinkondoth@gmail.com>
2026-05-12 12:03:07 +0530

    deleting old folder

--- SPI_slave_agent/SPI_sequencer.sv
+++
@@ -1,8 +0,0 @@
-class spi_sequencer extends uvm_sequencer #(spi_xtn);
-	`uvm_component_utils(spi_sequencer)
-
-	function new(string name = "spi_sequencer",uvm_component parent);
-		super.new(name,parent);
-	endfunction : new
-
-endclass : spi_sequencer
